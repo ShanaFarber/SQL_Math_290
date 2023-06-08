1f1646aaa94d05ab592a5a39eed7e50b0e5ce0ac
ShanaFarber <98992500+ShanaFarber@users.noreply.github.com>
2023-06-07 20:49:16 -0400

    update taxi trips

--- 2018_Yellow_Taxi_Data/distinct_count.sql
+++ 2018_Yellow_Taxi_Data/distinct_count.sql
@@ -1,106 +1,37 @@
 -- returns count of distinct values for each column
-select 'VendorID' as column_name, count(*) as distinct_records
-	from(
-		select distinct "VendorID" 
-		from "2018_Yellow_Taxi_Trip_Data"  
-	)as temp1
-union
-select 'tpep_pickup_datetime', count(*)
-	from(
-		select distinct "tpep_pickup_datetime" 
-		from "2018_Yellow_Taxi_Trip_Data" 
-	)as temp2
-union
-select 'tpep_dropoff_datetime', count(*)
-	from(
-		select distinct "tpep_dropoff_datetime" 
-		from "2018_Yellow_Taxi_Trip_Data"  
-	)as temp3
-union
-select 'passenger_count', count(*)
-	from(
-		select distinct "passenger_count"
-		from "2018_Yellow_Taxi_Trip_Data" 
-	)as temp4
-union
-select 'trip_distance', count(*)
-	from(
-		select distinct "trip_distance" 
-		from "2018_Yellow_Taxi_Trip_Data" 
-	)as temp5
-union
-select 'PULocationID', count(*)
-	from(
-		select distinct "PULocationID" 
-		from "2018_Yellow_Taxi_Trip_Data"  
-	)as temp6
-union
-select 'DOLocationID', count(*)
-	from(
-		select distinct "DOLocationID" 
-		from "2018_Yellow_Taxi_Trip_Data" 
-	)as temp7
-union
-select 'ratecodeID', count(*)
-	from(
-		select distinct "ratecodeID" 
-		from "2018_Yellow_Taxi_Trip_Data"  
-	)as temp8
-union
-select 'store_and_fwd_flag', count(*)
-	from(
-		select distinct "store_and_fwd_flag" 
-		from "2018_Yellow_Taxi_Trip_Data"  
-	)as temp9
-union
-select 'payment_type', count(*)
-	from(
-		select distinct "payment_type" 
-		from "2018_Yellow_Taxi_Trip_Data"  
-	)as temp10
-union
-select 'fare_amount', count(*)
-	from(
-		select distinct "fare_amount" 
-		from "2018_Yellow_Taxi_Trip_Data"  
-	)as temp11
-union
-select 'extra', count(*)
-	from(
-		select distinct "extra"
-		from "2018_Yellow_Taxi_Trip_Data" 
-	)as temp12
-union
-select 'mta_tax', count(*)
-	from(
-		select distinct "mta_tax" 
-		from "2018_Yellow_Taxi_Trip_Data"  
-	)as temp13
-union
-select 'improvement_surcharge', count(*)
-	from(
-		select distinct "improvement_surcharge" 
-		from "2018_Yellow_Taxi_Trip_Data" 
-	)as temp14
-union
-select 'tip_amount', count(*)
-	from(
-		select distinct "tip_amount" 
-		from "2018_Yellow_Taxi_Trip_Data" 
-	)as temp15
-union
-select 'tolls_amount', count(*)
-	from(
-		select distinct "tolls_amount" 
-		from "2018_Yellow_Taxi_Trip_Data" 
-	)as temp16
-union
-select 'total_amount', count(*)
-	from(
-		select distinct "total_amount" 
-		from "2018_Yellow_Taxi_Trip_Data"  
-	)as temp17
-;
+select 'VendorID' as column_name, count(distinct "VendorID") as distinct_records from "2018_Yellow_Taxi_Trip_Data"
+	union
+select 'tpep_pickup_datetime' as column_name, count(distinct "tpep_pickup_datetime") as distinct_records from "2018_Yellow_Taxi_Trip_Data"
+	union
+select 'tpep_dropoff_datetime' as column_name, count(distinct "tpep_dropoff_datetime") as distinct_records from "2018_Yellow_Taxi_Trip_Data"
+	union
+select 'Passenger_count' as column_name, count(distinct "passenger_count") as distinct_records from "2018_Yellow_Taxi_Trip_Data"
+	union
+select 'Trip_distance' as column_name, count(distinct "trip_distance") as distinct_records from "2018_Yellow_Taxi_Trip_Data"
+	union
+select 'PULocationID' as column_name, count(distinct "PULocationID") as distinct_records from "2018_Yellow_Taxi_Trip_Data"
+	union
+select 'DOLocationID' as column_name, count(distinct "DOLocationID") as distinct_records from "2018_Yellow_Taxi_Trip_Data"
+	union
+select 'RateCodeID' as column_name, count(distinct "ratecodeID" ) as distinct_records from "2018_Yellow_Taxi_Trip_Data"
+	union
+select 'Store_and_fwd_flag' as column_name, count(distinct "store_and_fwd_flag") as distinct_records from "2018_Yellow_Taxi_Trip_Data"
+	union
+select 'Payment_type' as column_name, count(distinct "payment_type") as distinct_records from "2018_Yellow_Taxi_Trip_Data"
+	union
+select 'Fare_amount' as column_name, count(distinct "fare_amount" ) as distinct_records from "2018_Yellow_Taxi_Trip_Data"
+	union
+select 'Extra' as column_name, count(distinct "extra")  as distinct_records from "2018_Yellow_Taxi_Trip_Data"
+	union
+select 'MTA_tax' as column_name, count(distinct "mta_tax") as distinct_records from "2018_Yellow_Taxi_Trip_Data"
+	union
+select 'improvement_surcharge' as column_name, count(distinct "improvement_surcharge") as distinct_records from "2018_Yellow_Taxi_Trip_Data"
+	union
+select 'Tip_amount' as column_name, count(distinct "tip_amount") as distinct_records from "2018_Yellow_Taxi_Trip_Data"
+	union
+select 'Tolls_amount' as column_name, count(distinct "tolls_amount") as distinct_records from "2018_Yellow_Taxi_Trip_Data"
+	union
+select 'Total_amount' as column_name, count(distinct "total_amount") as distinct_records from "2018_Yellow_Taxi_Trip_Data";
 
 -- returns count of all the distinct combinations of rows 
-select count(*) as distinct_observation_count from (select distinct * from taxi_fares) as sub_query;
+select count(*) as distinct_observation_count from (select distinct * from "2018_Yellow_Taxi_Trip_Data") as sub_query;
